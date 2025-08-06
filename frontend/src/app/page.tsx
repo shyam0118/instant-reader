51b3ffed59b4721ed96db7467e599dbcbd1a4dcc
Shyamalendu Nayak <shyamalendunayak0118@gmail.com>
2025-08-06 16:01:39 +0530

    Landing page hero section added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,8 @@
+import BeamsBackground from "@/components/kokonutui/beams-background";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Instant reader</div>
+    <BeamsBackground />
   );
 }
